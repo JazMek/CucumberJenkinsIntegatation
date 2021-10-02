eb4b38416c71767136c92f5d6bfd2e477abe7c6e
kmekdoud <karimmekdoud@gmail.com>
2021-10-02 17:23:11 -0400

    jjjj

--- src/test/java/stepDefinations/StepDf.java
+++ src/test/java/stepDefinations/StepDf.java
@@ -22,22 +22,17 @@ public class StepDf extends AmazonHomePage {
     }
     @Given("the user lands to the Amazon home page")
     public void the_user_lands_to_the_amazon_home_page() {
-        System.out.println(driver.getTitle());
-        try {
-            System.out.println(loadProperties("config.properties").getProperty("ImplicitlyWaitTime"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("User lands on : "+driver.getTitle());
 
     }
     @When("user enter {string}")
     public void user_enter(String input) {
-
-        SearchBox.sendKeys(input);
-        SearchBoxButton.click();
+        System.out.println("User enter "+input);
+//        SearchBox.sendKeys(input);
+//        SearchBoxButton.click();
     }
     @Then("user should see a corespending {string}")
-    public void user_should_see_a_corespending(String string) {
-        System.out.println(driver.getTitle());
+    public void user_should_see_a_corespending(String corespending) {
+        System.out.println("User should see : "+ corespending);
     }
 }
